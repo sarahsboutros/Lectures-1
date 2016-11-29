08fa91f6e0632a3295344148728fc328e04b0fbd
nealterrell <neal.terrell@csulb.edu>
2016-11-29 11:41:54 -0800

    Puzzle description image.

--- JavaScript/async_demo/routes/demo.js
+++ JavaScript/async_demo/routes/demo.js
@@ -33,15 +33,15 @@ router.get('/', function(req, res, next) {
 
 // A different approach using promises.
 // This code is not real, it is just a demo of ideas.
-/*
+
 
 Mongo.connect(url)
     .then(insertSomething)
     .then(deleteSomething)
     .then(logStuff);
-*/
+
 // What if I have a few promises and I want to wait for all to resolve?
-/*
+
 var p1 = checkLoginInformation();
 var p2 = downloadAvatarImage();
 
@@ -50,14 +50,14 @@ Promise.all([p1, p2])
     .then(function(vals) {
         addToLog(vals[0], vals[1]);
     });
-*/
+
 
 
 // This style of promises still requires lots of function callbacks. Instead of all the .then() stuff,
 // why can't we just declare a variable and have the program pause until the value is resolved?
 // That's where the "async/await" pattern comes in.
 
-/*
+
 var asyncLogin = checkLoginInformation(); // this does not return a real value, just the placeholder like a promise.
 var asyncDownload = downloadAvatarImage();
 
@@ -66,7 +66,7 @@ var loginResult = await asyncLogin;
 var downloadResult = await asyncDownload;
 
 var logResult = await addToLog(loginResult, downloadResult);
-*/
+
 
 
 
